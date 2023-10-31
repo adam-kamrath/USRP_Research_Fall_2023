100a532b3672c4bad8a135e20cc13ece8099dc74
adamkamrath <adamkamrath@outlook.com>
2023-10-31 11:52:14 -0500

    Updated app to add more options, and made transmitting more accurate

--- Applications/Transmit/Functions/generateSine.m
+++ Applications/Transmit/Functions/generateSine.m
@@ -1,8 +1,5 @@
-function sineData = generateSine(fs, num_of_samples)
-    time_scope = timescope(SampleRate=fs);
-    sine = dsp.SineWave(SamplesPerFrame=num_of_samples, SampleRate=fs, Frequency=166.66, ComplexOutput=true);
+function sineData = generateSine(fs, num_of_samples, frequency, complex)
+    sine = dsp.SineWave(SamplesPerFrame=num_of_samples, SampleRate=fs, Frequency=frequency, ComplexOutput=complex);
     sineData = sine();
-    % time_scope(sineData);
     release(sine);
-    % release(time_scope);
 end
--- StandaloneScripts/SignalFileWriter.m
+++ StandaloneScripts/SignalFileWriter.m
@@ -3,7 +3,7 @@ clc;
 center_frequency = 13.56e06;
 sampling_rate = 2e+06;
 decimation_factor = round(100000000/sampling_rate);
-file_name = 'System_Information.bb';
+file_name = 'Transmit_Test.bb';
 file_path = append('.\Signals\', file_name);
 
 %Create the receiver object
@@ -12,31 +12,26 @@ rx = comm.SDRuReceiver(...
                     IPAddress = '192.168.10.2', ...
                     OutputDataType = "double", ...
                     DecimationFactor = decimation_factor, ...
-                    Gain = 3, ...
+                    Gain = 0, ...
                     CenterFrequency = center_frequency, ...
-                    SamplesPerFrame = 500000);
+                    SamplesPerFrame = 1000);
 
 %Create the file writer object
 rxWriter = comm.BasebandFileWriter(file_path, ...
     SampleRate=sampling_rate, ...
     CenterFrequency=center_frequency);
 
-%Create Time Scope object
-time_scope = timescope(SampleRate=sampling_rate, Position=[1000,100,800,350]);
-
 %Plot 1 seconds of data, one frame at a time
-disp("Reception Started");
 frameduration = (rx.SamplesPerFrame)/(sampling_rate);
 time = 0; 
-while time < 4
+disp("Reception Started");
+while time < 5
   frame = rx();
   rxWriter(frame);
-  time_scope(frame)
   time = time + frameduration;
 end
 disp("Reception Stopped");
 
 %Release instances
 release(rxWriter);
-release(time_scope); 
 release(rx);
